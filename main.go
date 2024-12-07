89d93b2cf9fcd97f460d1af63a3d934fef84eb9f
NectoT <nectot2@gmail.com>
2024-12-07 19:13:06 +0300

    replace easily visible placeholder names

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ func main() {
 	// 'BackgroundColour' is the background colour of the window.
 	// 'URL' is the URL that will be loaded into the webview.
 	app.NewWebviewWindowWithOptions(application.WebviewWindowOptions{
-		Title: "Window 1",
+		Title: "Stopwatch tracker",
 		Mac: application.MacWindow{
 			InvisibleTitleBarHeight: 50,
 			Backdrop:                application.MacBackdropTranslucent,
